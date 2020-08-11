3d33f4fb03cacf28de763356c96183577e2c0c81
Daniel D <daniel@cyberwatch.fr>
2020-08-11 09:27:23 +0200

    Add new CI process

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='CyberWatch Api Tools.',
     long_description=open('README.md').read().strip(),
     long_description_content_type="text/markdown",
-    version='1.1.2',
+    version='1.1.8',
     author='CyberWatch SAS',
     author_email='support-it+api@cyberwatch.fr',
     license='MIT',
